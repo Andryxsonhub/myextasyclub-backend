084f54a7794ca534ce535d6865c35adc77b0efbf
Andryxsonhub <emailandersonsantos2023@gmail.com>
2025-09-23 16:38:57 -0300

    fix: Reseta e recria o histórico de migrações

--- prisma/migrations/20250923193610_init/migration.sql
+++ prisma/migrations/20250923193610_init/migration.sql
@@ -1,37 +1,35 @@
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
-    `bio` VARCHAR(191) NULL,
+    `bio` TEXT NULL,
     `profile_picture_url` VARCHAR(191) NULL,
+    `location` VARCHAR(191) NULL,
+    `gender` VARCHAR(191) NULL,
     `interests` VARCHAR(191) NULL,
     `desires` VARCHAR(191) NULL,
     `fetishes` VARCHAR(191) NULL,
     `favorited_suggestions` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
-    `gender` VARCHAR(191) NULL,
-    `last_seen_at` DATETIME(3) NULL,
-    `certification_level` INTEGER NOT NULL DEFAULT 0,
+    `lastSeenAt` DATETIME(3) NULL,
 
-    UNIQUE INDEX `User_email_key`(`email`),
-    UNIQUE INDEX `User_name_key`(`name`),
+    UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `users_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Post` (
+CREATE TABLE `posts` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `content` VARCHAR(191) NOT NULL,
-    `media_url` VARCHAR(191) NULL,
+    `content` TEXT NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
-    `author_id` INTEGER NOT NULL,
+    `authorId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Post` ADD CONSTRAINT `Post_author_id_fkey` FOREIGN KEY (`author_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `posts` ADD CONSTRAINT `posts_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
